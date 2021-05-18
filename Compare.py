b44c5816fcb8c4cc2ec2686410e8a5dd345b5109
sb1990gtr <82040359+sb1990gtr@users.noreply.github.com>
2021-05-18 08:51:50 +0200

    Update Compare.py

--- Compare.py
+++ Compare.py
@@ -11,9 +11,10 @@ from nltk.corpus import reuters
 
 label_topics="NoLabel" #possible values: "LabelAndSave", "Load", "LoadGoogle", "NoLabel", "LabelAndSaveGoogle"
 le = preprocessing.LabelEncoder()
-base_path = "/Users/sb1990gtr/Documents/Basti/Promotion/Uni/Veröffentlichung_2/LDA_LIME/R52_Experiment/src/lime_topic_final/"
+base_path = ""
 
 sys.path.append(os.path.abspath(base_path))
+#this is an external library: https://github.com/andifunke/topic-labeling
 import AutomatedTopicLabeling as topiclabeler
 
 dataset_test_all_terms = pd.read_csv(base_path + "r52-test-all-terms.txt", delimiter='\t', header=None)
@@ -33,26 +34,10 @@ all_labels_unique = pd.unique(dataset_complete.labels)
 class_names = [label for label in train_labels_numerical.classes_]
 
 
-#vectorizer = sklearn.feature_extraction.text.TfidfVectorizer(lowercase=False)
 vectorizer = sklearn.feature_extraction.text.CountVectorizer()
 train_vectors = vectorizer.fit_transform(dataset_train.features)
 test_vectors = vectorizer.transform(dataset_test.features)
 all_vectors = vectorizer.transform(dataset_complete.features)
-
-#from sklearn import tree
-#clf = tree.DecisionTreeClassifier(criterion = 'entropy').fit(train_vectors, dataset_train.labels)
-
-#from sklearn.naive_bayes import MultinomialNB
-#nb = MultinomialNB(alpha=.01)
-#clf = nb.fit(train_vectors, dataset_train.labels)
-
-
-#from sklearn.linear_model import LogisticRegression
-#clf = LogisticRegression(random_state=0).fit(train_vectors, dataset_train.labels)
-#coef_log_reg = clf.coef_.T
-#class_names = clf.classes_
-#feature_names = vectorizer.get_feature_names()
-#coeff_df = pd.DataFrame(coef_log_reg, columns=class_names, index=feature_names)
 
 # SGBoost
 import xgboost as xgb
@@ -60,9 +45,7 @@ xg = xgb.XGBClassifier(max_depth=5, learning_rate=0.1, n_estimators=10,
                               silent=True, objective='reg:logistic', booster='gbtree')
 clf = xg.fit(train_vectors, dataset_train.labels)
 
-#from sklearn.svm import SVC
-#clf = SVC(gamma='auto', probability=True, kernel="linear")
-#clf.fit(train_vectors, dataset_train.labels)
+
 
 #predict & evaluate
 pred = clf.predict(test_vectors)
@@ -77,25 +60,12 @@ c = make_pipeline(vectorizer, clf)
 # choose the instance
 dataset_test[dataset_test["labels"] == 'acq']
 idx = 371#1081#95
-# used in paper: 645#566 auch spannend #456  #345 best #332 guter Vergleich, zeigt mental calc und interaction ala short term  #321
 print("Original instance: " + str(dataset_test_all_terms[1][idx]))
 print("Original instance label: " + str(dataset_test_all_terms[0][idx]))
 
 
 
-#exp.as_pyplot_figure(label=exp.available_labels()[0])
-#exp.save_to_file('lime_word.html', labels=[exp.available_labels()[0]])
-
-#print ('Explanation for class %s' % class_names[0])
-#print ('\n'.join(map(str, exp.as_list(label=0))))
-# print ()
-# print ('Explanation for class %s' % class_names[17])
-# print ('\n'.join(map(str, exp.as_list(label=17))))
-
-#exp.show_in_notebook(text=False, lbels=(0,))
-
 import sys, os
-sys.path.append("/Users/sb1990gtr/Documents/Basti/Promotion/Uni/Veröffentlichung_2/LDA_LIME/R52_Experiment/src/")
 
 from lime_topic_final.lime_text_topics import PreProcessor
 
@@ -105,10 +75,6 @@ from lime_topic_final.lime_text_topics import PreProcessor
 data_processor = PreProcessor()
 data_preprocessed = data_processor.preprocess_string(dataset_test.features[idx])
 
-#print('\n--- the origional document---\n')
-#print(dataset_test.features[idx])
-#print('\n--- the preprocessed document---\n')
-#print(data_preprocessed)
 
 import gensim
 from gensim.test.utils import datapath
@@ -120,7 +86,6 @@ temp_file = datapath(base_path + "lda_reuters_19_final.model")
 # Load a potentially pretrained model from disk.
 lda_model = LdaModel.load(temp_file)
 
-#prüfen, ob topics gelabeld werden sollen
 if (label_topics == "LabelAndSave"):
     topics = []
     for topic_id, topic in enumerate(lda_model.show_topics(num_topics=lda_model.num_topics)):
@@ -130,7 +95,6 @@ if (label_topics == "LabelAndSave"):
             #print(str(word))
             most_imp_words.append(word)
 
-        #print(str(topic_id), str(lda_model.show_topic(topic_id, topn=10)[0]))
         topics_labels = topiclabeler.remove_duplicates(topiclabeler.lemmatize_all(topiclabeler.get_relevant_articles(most_imp_words)))
         topics.append('topic #'+str(topic[0]) + ": " + str(topics_labels))
         #save topics_list
@@ -147,10 +111,8 @@ elif (label_topics=="LabelAndSaveGoogle"):
         most_imp_words = []
         words_prob_per_topic = lda_model.show_topic(topic[0], topn=5)
         for word, proba in words_prob_per_topic:
-            # print(str(word))
             most_imp_words.append(word)
 
-        # print(str(topic_id), str(lda_model.show_topic(topic_id, topn=10)[0]))
         topics_labels = googleLabeler.pipeline(most_imp_words)
 
         topics.append('topic #' + str(topic[0]) + ": " + str(topics_labels))
@@ -178,40 +140,13 @@ else:
 id2word = Dictionary.load_from_text(base_path + 'dict.txt')
 
 
-#print(f'Number of topics: {lda_model.num_topics}')
-# print top words for one topic
-#lda_model.print_topics(1)
-
-# minimum_probability = 0.001
-#
-#
-# def word_to_topics(word):
-#     """
-#     Maps a word on its corresponding topics
-#     :param word: the word that is searched for
-#     :return: list of topics
-#     """
-#
-#     word_lem_list = data_processor.lemmatize(word)
-#     if len(word_lem_list) > 0:
-#         word_lem = word_lem_list[0]
-#         if word_lem in id2word.token2id:
-#             word_id = id2word.token2id[word_lem]
-#             return [x[0] for x in lda_model.get_term_topics(word_id, minimum_probability=minimum_probability)]
-#         else:
-#             return []
-#     else:
-#         return []
-
 def word_to_topics(word):
     """
     Maps a word on its corresponding topics
     :param word: the word that is searched for
     :return: list of topics
     """
-    #word_lem_list = data_processor.lemmatize(word)
-    #if len(word_lem_list) > 0:
-        #word_lem = word_lem_list[0]
+
         if word in id2word.token2id:
                 word_id = id2word.token2id[word]
                 # so im paper ecml verwernet: if lda_model.get_term_topics(word_id, minimum_probability=0.000000000000001):
@@ -236,16 +171,11 @@ def word_to_topics(word):
 
 from lime.lime_text import LimeTextExplainer
 from lime_topic_final.lime_text_topics import LimeTextByTopicsExplainer
-sys.path.append("/Users/sb1990gtr/Documents/Basti/Promotion/Uni/Veröffentlichung_2/LDA_LIME/R52_Experiment/src/lime_topic_final/")
 from lime_topic_final.lime_topic_words import LimeTextByTopicSampledWordsExplainer
 
 # explain the instance using word-based standard LIME
 explainer = LimeTextExplainer(class_names=class_names, feature_selection="forward_selection", random_state=54321, verbose=False)
 exp = explainer.explain_instance(dataset_test.features[idx], c.predict_proba, num_features=80, top_labels=1)
-
-# explain the instance using word-based LIME-SFA
-explainer_topic_words = LimeTextByTopicSampledWordsExplainer(class_names=class_names, consider_all_words=True, word_to_topics=word_to_topics, topics=topics, feature_selection="forward_selection", random_state=54321, verbose=False)
-exp_topic_words = explainer_topic_words.explain_instance(dataset_test.features[idx], c.predict_proba, num_features=12, top_labels=1)
 
 # explain the instance using topicLIME
 explainer_mod = LimeTextByTopicsExplainer(class_names=class_names, consider_all_words=False, word_to_topics=word_to_topics, topics=topics, feature_selection="forward_selection", random_state=54321, verbose=False)
@@ -263,9 +193,6 @@ def extractMostImportantWords(noOfWords=10):
     relevant_words_sorted = relevant_words.sort_values(ascending=False)[:noOfWords]
     return relevant_words_sorted
 
-#print("intercept_mod : " + str(exp_mod.intercept))
-#print("local prediction_mod: " + str(exp_mod.local_pred))
-#print("prediction score (R square)_mod: " + str(exp_mod.score))
 
 #Show different explanations
 print('Document id: %d' % idx)
@@ -285,13 +212,6 @@ for x in exp.available_labels():
     print()
 
 
-print('\n\n********************* LIME-SFA ***********************\n\n')
-for x in exp_topic_words.available_labels():
-    print('Topic-word explanation for class %s' % class_names[x])
-    print('\n'.join(map(str, exp_topic_words.as_list(label=x))))
-    print()
-
-
 print('\n\n********************* topicLIME ***********************\n\n')
 for x in exp_mod.available_labels():
     print('Topic-based explanation for class %s' % class_names[x])
@@ -303,7 +223,7 @@ for x in exp_mod.available_labels():
 print(str(lda_model.get_document_topics(id2word.doc2bow(dataset_test.features[idx].split()))))
 
 
-########################## explanation and fidelity evaluation - topicLIME vs. LIME vs. LIME-SFA ###################################
+########################## explanation and fidelity evaluation - topicLIME vs. LIME ###################################
 def classifyWOExplUnit_words(instance, explanation_unit):
     # create vector representation for given instance
     # instance_vector = vectorizer.transform(pd.Series(dataset_test.features[idx]))
@@ -375,13 +295,10 @@ def classifyWOExplUnit_topics(instance, explanation_unit):
 
 # parameter definitions for eval run #
 # n_eu = 4 # the number of LIME topic explanation units that shall be checked
-# # TODO: Permutiere a) Anzahl n_eu, b) Anzahl der Topics aus dem LDA Model und c) den BASE Classifier d) ziehe UNKNOWN words hinzu
 #
 #
 def eval_topics(n_eu, instance):
-    # definiere und setze Gesamtfehler_topic auf 0
-    # # für jede eu in n_eu aus LIME topic Erklärung
-    #build explanation object
+
     exp_mod = explainer_mod.explain_instance(instance, c.predict_proba, num_features=n_eu, top_labels=1)
     #exp_mod = explainer_mod.explain_instance(dataset_test.features[idx], c.predict_proba, num_features=10, top_labels=1)
 
@@ -404,9 +321,7 @@ def eval_topics(n_eu, instance):
     return conf_difference_topic_local, num_words_in_topic
 #
 def eval_words(num_words, instance):
-    # definiere und setze Gesamtfehler_wort auf 0
-    # # für jede eu in n_eu aus LIME wort Erklärung
-    # build explanation object for according instance
+
     exp = explainer.explain_instance(instance, c.predict_proba, num_features=num_words, top_labels=1)
     #exp = explainer.explain_instance(dataset_test.features[idx], c.predict_proba, num_features=40, top_labels=1)
 
@@ -422,24 +337,6 @@ def eval_words(num_words, instance):
     #conf_difference_word = classifyWOExplUnit_words(dataset_test.features[idx], [expl[0]])
     return conf_difference_word_local
 
-#
-# #run eval
-# difference_topic_total = 0
-# difference_word_total = 0
-#
-# for instance in dataset_test.features:
-#     conf_difference_topic, num_words_in_topic = eval_topics(instance)
-#     #mean_error_topic_instance, num_words_in_topic = eval_topics(dataset_test.features[idx])
-#
-#     difference_topic_total += conf_difference_topic
-#
-#     difference_word_instance = eval_words(num_words_in_topic, instance)
-#     #mean_error_word_instance = eval_words(num_words_in_topic, dataset_test.features[idx])
-#
-#     difference_word_total += difference_word_instance
-#
-# mean_difference_topic_total = difference_topic_total / len(dataset_test.features)
-# mean_difference_word_total = difference_word_total / len(dataset_test.features)
 
 ########################## explanation and fidelity evaluation - topic_words ###################################
 def classifyWOExplUnit_topic_words(instance, explanation_unit):
@@ -456,8 +353,6 @@ def classifyWOExplUnit_topic_words(instance, explanation_unit):
     #orig_instance_split = dataset_test.features[idx].split()
     orig_instance_split = instance.split()
 
-    # explanation_unit = ["ship", "cranes", "development","crane"]
-    # explanation_unit = ["port"]
     document_wo_eu = [word for word in orig_instance_split if word.lower() not in explanation_unit]
     document_wo_eu_str = ' '.join(document_wo_eu)
 
@@ -481,8 +376,7 @@ def classifyWOExplUnit_topic_words(instance, explanation_unit):
 n_eu = 5
 
 def eval_topic_words(number_of_words, instance):
-    # definiere und setze Gesamtfehler_wort auf 0
-    # build explanation object for according instance
+
     exp_top_words = explainer_topic_words.explain_instance(instance, c.predict_proba, num_features=number_of_words, top_labels=1)
     #exp = explainer.explain_instance(dataset_test.features[idx], c.predict_proba, num_features=40, top_labels=1)
 
@@ -521,7 +415,7 @@ mean_difference_topics_total = difference_topics_total / len(dataset_test.featur
 mean_difference_words_total = difference_words_total / len(dataset_test.features)
 
 
-# #Analyse der Local Fidelity der Explanation models generated by LIME, LIME-SFA and topicLIME
+# #Analysis of Local Fidelity 
 # error_word = 0
 # #error_topic = 0
 # error_topic_word = 0
